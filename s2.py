ffe0903bd2bab7ef8fc3fe9665c82fc2f6add4a1
msa <msai.sadeghi@gmail.com>
2023-10-31 20:26:59 +0330

    s

--- s2.py
+++ s2.py
@@ -1,1 +1,24 @@
-print("hello")
+# print("hello")
+
+# print(1+6)
+# print(1/6)
+# print(1-6)
+# print(1*6)
+
+# print(8//3)
+# print(8 % 3)
+
+# a = 12
+# print(a)
+
+name = "elin"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+
+
+print(len(name))
+
+name = input("Enter your name: ")
+print("your name is:", name)
